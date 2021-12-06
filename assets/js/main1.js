756723bdebf765a8595223f5d800b3c468366bc0
pavan kumar <85506125+pavanbattu@users.noreply.github.com>
2021-12-06 13:37:59 +0530

    portfolio

---
+++ assets/js/main1.js
@@ -0,0 +1,20 @@
+
+
+
+
+var main = document.querySelector('.main');
+var nav = document.querySelector('.nav');
+
+var offset = main.offsetHeight-nav.offsetHeight;
+
+window.onscroll = function(){
+
+  if(window.pageYOffset > offset){
+    nav.classList.remove('bottom-nav');
+    nav.classList.add('top-nav');
+  }
+  else{
+    nav.classList.add('bottom-nav');
+    nav.classList.remove('top-nav');
+  }
+}
